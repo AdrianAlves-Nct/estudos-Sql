49888fee5b36295cc47fa9a88cb6d54195fcd16e
Adrian M. do Nascimento Alves <adrianmoizes123@gmail.com>
2025-09-09 19:02:40 -0300

    feito ep04 - 1ex

--- ep04/join.sql
+++ ep04/join.sql
@@ -6,7 +6,7 @@ onde coluna da FK 1 for igual a essa coluna da PK 2*/
 SELECT t1.*, t2.product_category_name
 FROM tb_order_items as t1
 LEFT JOIN tb_products as t2
-ON t1.product_id = t2.product_id
+        ON t1.product_id = t2.product_id
 
 
 -- Qual o top 10 receita de cada categoria de produto?
@@ -14,7 +14,7 @@ SELECT t2.product_category_name,
         sum(t1.price) as receita
 FROM tb_order_items as t1
 LEFT JOIN tb_products as t2
-ON t1.product_id = t2.product_id
+        ON t1.product_id = t2.product_id
 GROUP by t2.product_category_name
 ORDER BY  sum(t1.price) DESC
 LIMIT 10
@@ -25,23 +25,23 @@ SELECT t2.product_category_name,
         count(DISTINCT t1.order_id) as qtd_pedidos
 FROM tb_order_items as t1
 LEFT JOIN tb_products as t2
-ON t1.product_id = t2.product_id
+        ON t1.product_id = t2.product_id
 GROUP by t2.product_category_name
 ORDER BY  count(*) DESC
 
 
 -- e considerando as vendas de 2017 e pedidos entregues
 SELECT t2.product_category_name, 
-        sum(t1.price) as receita,  
+        round(sum(t1.price),2) as receita,  
         count(*) as total_vendidos, 
         count(DISTINCT t1.order_id) as qtd_pedidos,
         t3.order_approved_at as data_aprovacao_pedido, 
         t3.order_status as status_venda
 FROM tb_order_items as t1
 LEFT JOIN tb_products as t2
-ON t1.product_id = t2.product_id
+        ON t1.product_id = t2.product_id
 LEFT JOIN tb_orders as t3
-ON t1.order_id = t3.order_id
+        ON t1.order_id = t3.order_id
 WHERE t3.order_approved_at LIKE "%2017%" AND t3.order_status = 'delivered'
 GROUP by t2.product_category_name
 ORDER BY count(*)  DESC
